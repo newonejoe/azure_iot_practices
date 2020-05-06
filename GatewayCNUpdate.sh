3d6dc12412e4bc8cc6fd11d82ae4bd53bf933fb3
newonejoe <newonejoe@gmail.com>
2020-05-06 11:44:29 +0800

    Add header

--- GatewayCNUpdate.sh
+++ GatewayCNUpdate.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+echo "Update Start"
 snap stop komax-iot-gateway
 snap remove komax-iot-gateway komax-gateway-certificates komax-iotedge
 snap install komax-gateway-certificates-cn 
 snap install komax-iotedge-cn
 snap install komax-iot-gateway
+echo "Update Finish, rebooting!"
 sudo reboot 
